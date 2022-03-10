4e7a0daf422fdeca2f9be55aeb17eb5f1f7123d1
Bruno <bruno.sa@sc.senai.br>
2022-03-10 15:47:54 -0300

    MovieCard Layout

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,7 +1,7 @@
-import '.styles.css';
-import {ReactComponent as StarFull} from 'assets/img/start-full.svg';
-import {ReactComponent as StarHalf} from 'assets/img/start-half.svg';
-import {ReactComponent as StarEmpty} from 'assets/img/start-empty.svg';
+import './styles.css';
+import {ReactComponent as StarFull} from 'assets/img/star-full.svg';
+import {ReactComponent as StarHalf} from 'assets/img/star-half.svg';
+import {ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
 
 function MovieStars() {
 
