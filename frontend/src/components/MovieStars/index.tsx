7dea1d2a46ef092ecd0dc448af241a9fe763537b
Bruno <bruno.sa@sc.senai.br>
2022-03-10 15:09:58 -0300

    MovieStars Layout

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,19 @@
+import '.styles.css';
+import {ReactComponent as StarFull} from 'assets/img/start-full.svg';
+import {ReactComponent as StarHalf} from 'assets/img/start-half.svg';
+import {ReactComponent as StarEmpty} from 'assets/img/start-empty.svg';
+
+function MovieStars() {
+
+    return(
+        <div className="dsmovie-stars-container">
+            <StarFull />
+            <StarFull />
+            <StarFull />
+            <StarHalf />
+            <StarEmpty />
+        </div>
+    );
+}
+
+export default MovieStars;
